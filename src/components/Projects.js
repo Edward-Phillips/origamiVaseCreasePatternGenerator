94c64876e2bc9bfd52db61f4221003ec4dddb057
Edward-Phillips <edd0ddi0@gmail.com>
2021-08-15 14:25:00 +0100

    initial projects page commit

---
+++ src/components/Projects.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import SVGDragAndDrop from "./Projects/SVGDragAndDrop/SVGDragAndDrop";
+// import SVGDisplay from './Projects/SVGDisplay';
+
+export default function Projects() {
+
+  return (
+    <section>
+      <div>
+        this is the Projects component, I'm going to use the github API to
+        populate this page, so again, going to need a backend to process that -
+        not sure what language to do the backend in, recently I have been
+        working mostly in javascript/react on the frontend and PHP (laravel) on
+        the backend. I kind of want to get good with Python so maybe something
+        in Django or Flask - or just use Express, it's what you know?
+      </div>
+    </section>
+  );
+}
