5375255b44076d21ab83688adc2ee1c1db75edfd
Edward-Phillips <edd0ddi0@gmail.com>
2021-08-19 10:59:24 +0100

    add default points, switch from lines to polyline, add ability to remove points as well as add them, add grid css to outer container

--- src/components/Projects/SVGDragAndDrop/SVGDragAndDropContainer.js
+++ src/components/Projects/SVGDragAndDrop/SVGDragAndDropContainer.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import SVGDragAndDrop from './SVGDragAndDrop';
+import './SVGDragAndDropContainer.css';
 
 export default function SVGContainer() {
 
-  const [points, setPoints] = React.useState({"point0": {cx: 50, cy: 50, key: "point0"}});
+  const [points, setPoints] = React.useState({"point0": {cx: 0, cy: 100, key: "point0"}, "point1": {cx: 50, cy: 100, key: "point1"}});
 
   const mapPointsToSVGPoints = () => {
-    return Object.keys(points).map((pointKey) => {
-    return (<SVGDragAndDrop coords={points[pointKey]} setCoords={updateValueInPointsObject} />)
-  });
+    return Object.keys(points).map((pointKey) => <SVGDragAndDrop coords={points[pointKey]} setCoords={updateValueInPointsObject} />);
   } 
 
   const updateValueInPointsObject = (key, value) => {
@@ -22,12 +21,42 @@ export default function SVGContainer() {
     points[newKey] = newPoint;
     setPoints({...points});
   }
+
+  const removeLastPointFromPointsObject = () => {
+    const lastKey = Object.keys(points).pop();
+    delete points[lastKey];
+    setPoints({...points});
+  }
+
+  const shouldAddPolyline= () => {
+    return Object.keys(points).length > 1;
+  }
+
+
+  const generatePolylinePointsStringFromPointsObject = () => {
+    const pointsString = Object.keys(points).map((pointKey) => {
+      return `${points[pointKey].cx},${points[pointKey].cy}`;
+    }).join(" ");
+    return pointsString;
+  }
+
+  const generatePolyLine = () => {
+    return <polyline points={generatePolylinePointsStringFromPointsObject()} fill="none" stroke="black" strokeWidth="2" strokeLinecap="round" />
+  }
+
+
   return (
-    <div className={"svgContainer"}>
-    <svg viewBox="0 0 100 100">
-      {mapPointsToSVGPoints()}
-    </svg>
-    <button onClick={addPointToPointsObject}>Add Point</button>
+    <div className="SVGOuterContainer">
+      <div className={"svgContainer"}>
+        <svg viewBox="0 0 100 100">
+          {shouldAddPolyline() ? generatePolyLine() : null}
+          {mapPointsToSVGPoints()}
+        </svg>
+      </div>
+      <div className="SVGControls">
+      <button onClick={addPointToPointsObject}>Add Point</button>
+      <button onClick={removeLastPointFromPointsObject}>Remove Last Point</button>
+      </div>
     </div>
   )
   
